258e763f3dd1efcd30ede1aa7b4554e96a7f9dba
TOliverK <kocsis.tamas.oliver@gmail.com>
2024-08-29 18:43:27 +0100

    Include benchmark test for Ask Vol and Best Bid

--- GoogleBenchmark/OrderBook_benchmark.cpp
+++ GoogleBenchmark/OrderBook_benchmark.cpp
@@ -6,14 +6,11 @@
 /*  This Google Benchmark file is for measuring separate function execution times.
  */
 
-// todo micro benchmarking:
-// - get best bid
-// - get volume between prices
 
 /*
- *  Micro Benchmark Addorder1: adding the same order
- *  For the prioQ database version, adding the same price is probably slower, so have more test where
- *  the price changes.
+ *  Micro Benchmark Addorder Test 1:
+ *  Adding the same order. For the prioQ database version, adding the same price is probably slower,
+ *  so have more test where the price changes.
  *  Order db empty at start.
  */
 static void BM_AddOrder_same_price(benchmark::State &state) {
@@ -34,7 +31,8 @@ static void BM_AddOrder_same_price(benchmark::State &state) {
 
 
 /*
- *  Micro Benchmark Addorder2: adding an order with random prices every iter, with range of prices in 3
+ *  Micro Benchmark Addorder Test 2:
+ *  Adding an order with random prices every iter, with range of prices in 3.
  *  Order db empty at start.
  */
 static void BM_AddOrder_RandomPrice_Range_3(benchmark::State &state) {
@@ -61,7 +59,8 @@ static void BM_AddOrder_RandomPrice_Range_3(benchmark::State &state) {
 
 
 /*
- *  Micro Benchmark Addorder3: Increase the price range to 10
+ *  Micro Benchmark Addorder Test 3:
+ *  Increase the price range to 10.
  *  Order db empty at start.
  */
 static void BM_AddOrder_RandomPrice_Range_10(benchmark::State &state) {
@@ -88,8 +87,8 @@ static void BM_AddOrder_RandomPrice_Range_10(benchmark::State &state) {
 
 
 /*
- *  Micro Benchmark Addorder4: Increase the price range to 10
- *  Order db has 10k size at the benchmark start.
+ *  Micro Benchmark Addorder Test 4:
+ *  Increase the price range to 10 Order db has 10k size at the benchmark start.
  */
 static void BM_AddOrder_RandomPrice_Range_10_DB10k(benchmark::State &state) {
     OrderBook orderBook;
@@ -126,154 +125,133 @@ static void BM_AddOrder_RandomPrice_Range_10_DB10k(benchmark::State &state) {
 
 
 /*
- *  Micro Benchmark Cancel Order 1: Adding 1 Order, then cancel a RANDOM order, while having 1k orders in the db
+ *  Micro Benchmark Cancel Order:
+ *  Adding 1 Order, then cancel a RANDOM order, while having a preloaded amount
+ *  of Orders. The amount changes with test input benchmark::state: 100 1k 10k 100k amount of preloaded orders in the
+ *  db, before we start adding, then canceling a random Order.
+ */
+static void BM_Cancel1_Random_Order_DB_(benchmark::State &state) {
+    OrderBook orderBook;
+    std::vector<int> OrderIDs(state.range(0), 0);  //save the id to know which one to cancel
+
+    // Random number generators
+    std::random_device rd;  // Random seed
+    std::mt19937 gen(rd()); // Mersenne Twister engine
+    std::uniform_int_distribution<> priceDistrib(99, 101);  // Price range: keep stock price stable: RANGE: 3
+    std::uniform_int_distribution<> quantityDistrib(50, 5000); // Define quantity range
+    std::uniform_int_distribution<> randomIDindex(0, OrderIDs.size() - 1); // define the range
+
+    int orderID = 1;
+    Order buyOrder = {OrderType::buy, 1, 100, 5};
+
+    //preload varied amount of orders, based on benchmark state input
+    for(int i=0; i< state.range(0); i++) {
+        orderBook.AddOrder(buyOrder);
+        OrderIDs[i]=orderID;
+        orderID++;
+        buyOrder.orderId = orderID;
+        buyOrder.price = priceDistrib(gen);
+        buyOrder.quantity = quantityDistrib(gen);
+    }
+
+
+    for (auto _: state) {
+        //Added overhead of pausing and resuming timer, could be ignored as the Cancel order time is awful at the moment
+        state.PauseTiming();
+        buyOrder.orderId = ++orderID;
+        orderBook.AddOrder(buyOrder); //keep adding orders so that we keep the 10k size
+        //Get random order id, add the new order id to its place to avoid deleting from middle of vector (would be O(n))
+        //int randomIndex = std::rand() % OrderIDs.size();
+        int randomIndex = randomIDindex(gen);
+        int randomOrderID = OrderIDs[randomIndex];
+        OrderIDs[randomIndex] = orderID;
+        state.ResumeTiming();
+
+        orderBook.CancelOrderbyId(randomOrderID);
+        benchmark::DoNotOptimize(orderBook);
+    }
+}
+
+/*
+ *  Micro Benchmark Get Best Bid 1
+ *  Price Range of preloaded messages matters, since if less orders are on present on the best level
+ *  less orders needs to be collected. (At least for current implementation)
+ */
+static void BM_GetBestBid_bidDB_(benchmark::State &state) {
+    OrderBook orderBook;
+
+    // Random number generators
+    std::random_device rd;  // Random seed
+    std::mt19937 gen(rd()); // Mersenne Twister engine
+    std::uniform_int_distribution<> priceDistrib(98, 100);  // Price range: keep stock price stable: RANGE: 3
+    std::uniform_int_distribution<> quantityDistrib(50, 5000); // Define quantity range
+
+    int orderID = 1;
+    Order buyOrder = {OrderType::buy, 1, 100, 5};
+
+    //preload varied amount of orders, based on benchmark state input
+    for(int i=0; i< state.range(0); i++) {
+        orderBook.AddOrder(buyOrder);
+        orderID++;
+        buyOrder.orderId = orderID;
+        buyOrder.price = priceDistrib(gen);
+        buyOrder.quantity = quantityDistrib(gen);
+    }
+
+    for (auto _: state) {
+        //Benchmark loop
+        benchmark::DoNotOptimize(orderBook);
+        orderBook.GetBestBidWithQuantity();
+    }
+}
+
+
+/*
+ *  Micro Benchmark Get Ask Volume Between Prices
  *
  */
-static void BM_Cancel1_Random_Order_DB1k(benchmark::State &state) {
+static void BM_GetAskVolume_askDB_(benchmark::State &state) {
     OrderBook orderBook;
-    std::vector<int> OrderIDs(1000, 0);  //save the id to know which one to cancel
 
     // Random number generators
     std::random_device rd;  // Random seed
     std::mt19937 gen(rd()); // Mersenne Twister engine
-    std::uniform_int_distribution<> priceDistrib(99, 101);  // Price range: keep stock price stable: RANGE: 3
+    std::uniform_int_distribution<> priceDistrib(98, 100);  // Price range: keep stock price stable: RANGE: 3
     std::uniform_int_distribution<> quantityDistrib(50, 5000); // Define quantity range
-    std::uniform_int_distribution<> randomIDindex(0, OrderIDs.size() - 1); // define the range
 
     int orderID = 1;
-    Order buyOrder = {OrderType::buy, 1, 100, 5};
+    Order askOrder = {OrderType::sell, 1, 100, 5};
 
-    //preload 1k orders, so that we actually have to look to cancel a random order
-    for(int i=0; i< 1000; i++) {
-        orderBook.AddOrder(buyOrder);
-        OrderIDs[i]=orderID;
-
-        //update id and price
+    //preload varied amount of orders, based on benchmark state input
+    for(int i=0; i< state.range(0); i++) {
+        orderBook.AddOrder(askOrder);
         orderID++;
-        buyOrder.orderId = orderID;
-        buyOrder.price = priceDistrib(gen);
-        buyOrder.quantity = quantityDistrib(gen);
+        askOrder.orderId = orderID;
+        askOrder.price = priceDistrib(gen);
+        askOrder.quantity = quantityDistrib(gen);
     }
 
-
     for (auto _: state) {
-        //Added overhead of pausing and resuming timer, could be ignored as the Cancel order time is awful at the moment
-        state.PauseTiming();
-        buyOrder.orderId = ++orderID;
-        orderBook.AddOrder(buyOrder); //keep adding orders so that we keep the 10k size
-        //Get random order id, add the new order id to its place to avoid deleting from middle of vector (would be O(n))
-        //int randomIndex = std::rand() % OrderIDs.size();
-        int randomIndex = randomIDindex(gen);
-        int randomOrderID = OrderIDs[randomIndex];
-        OrderIDs[randomIndex] = orderID;
-        state.ResumeTiming();
-
-        orderBook.CancelOrderbyId(randomOrderID);
+        //Benchmark loop
         benchmark::DoNotOptimize(orderBook);
+        orderBook.GetVolumeBetweenPrices(99, 100);
     }
 }
 
-/*
- *  Micro Benchmark Cancel Order 2: Adding 1 Order, then cancel a RANDOM order, while having 10k orders in the db
- */
-static void BM_Cancel1_Random_Order_DB10k(benchmark::State &state) {
-    OrderBook orderBook;
-    std::vector<int> OrderIDs(10000, 0);  //save the id to know which one to cancel
-
-    // Random number generators
-    std::random_device rd;  // Random seed
-    std::mt19937 gen(rd()); // Mersenne Twister engine
-    std::uniform_int_distribution<> priceDistrib(99, 101);  // Price range: keep stock price stable: RANGE: 3
-    std::uniform_int_distribution<> quantityDistrib(50, 5000); // Define quantity range
-    std::uniform_int_distribution<> randomIDindex(0, OrderIDs.size() - 1); // define the range
-
-    int orderID = 1;
-    Order buyOrder = {OrderType::buy, 1, 100, 5};
-
-    //preload 10k orders, so that we actually have to look to cancel a random order
-    for(int i=0; i< 10000; i++) {
-        orderBook.AddOrder(buyOrder);
-        OrderIDs[i]=orderID;
-
-        //update id and price
-        orderID++;
-        buyOrder.orderId = orderID;
-        buyOrder.price = priceDistrib(gen);
-        buyOrder.quantity = quantityDistrib(gen);
-    }
-
-
-    for (auto _: state) {
-        //Added overhead of pausing and resuming timer, could be ignored as the Cancel order time is awful at the moment
-        state.PauseTiming();
-        buyOrder.orderId = ++orderID;
-        orderBook.AddOrder(buyOrder); //keep adding orders so that we keep the 10k size
-        //Get random order id, add the new order id to its place to avoid deleting from middle of vector (would be O(n))
-        //int randomIndex = std::rand() % OrderIDs.size();
-        int randomIndex = randomIDindex(gen);
-        int randomOrderID = OrderIDs[randomIndex];
-        OrderIDs[randomIndex] = orderID;
-        state.ResumeTiming();
-
-        orderBook.CancelOrderbyId(randomOrderID);
-        benchmark::DoNotOptimize(orderBook);
-    }
-}
-
-/*
- *  Micro Benchmark Cancel Order 3: Adding 1 Order, then cancel a RANDOM order, while having 100k orders in the db
- */
-static void BM_Cancel1_Random_Order_DB100k(benchmark::State &state) {
-    OrderBook orderBook;
-    std::vector<int> OrderIDs(100000, 0);  //save the id to know which one to cancel
-
-    // Random number generators
-    std::random_device rd;  // Random seed
-    std::mt19937 gen(rd()); // Mersenne Twister engine
-    std::uniform_int_distribution<> priceDistrib(99, 101);  // Price range: keep stock price stable: RANGE: 3
-    std::uniform_int_distribution<> quantityDistrib(50, 5000); // Define quantity range
-    std::uniform_int_distribution<> randomIDindex(0, OrderIDs.size() - 1); // define the range
-
-    int orderID = 1;
-    Order buyOrder = {OrderType::buy, 1, 100, 5};
-
-    //preload 100k orders, so that we actually have to look to cancel a random order
-    for(int i=0; i< 100000; i++) {
-        orderBook.AddOrder(buyOrder);
-        OrderIDs[i]=orderID;
-
-        //update id and price
-        orderID++;
-        buyOrder.orderId = orderID;
-        buyOrder.price = priceDistrib(gen);
-        buyOrder.quantity = quantityDistrib(gen);
-    }
-
-
-    for (auto _: state) {
-        //Added overhead of pausing and resuming timer, could be ignored as the Cancel order time is awful at the moment
-        state.PauseTiming();
-        buyOrder.orderId = ++orderID;
-        orderBook.AddOrder(buyOrder); //keep adding orders so that we keep the 10k size
-        //Get random order id, add the new order id to its place to avoid deleting from middle of vector (would be O(n))
-        //int randomIndex = std::rand() % OrderIDs.size();
-        int randomIndex = randomIDindex(gen);
-        int randomOrderID = OrderIDs[randomIndex];
-        OrderIDs[randomIndex] = orderID;
-        state.ResumeTiming();
-
-        orderBook.CancelOrderbyId(randomOrderID);
-        benchmark::DoNotOptimize(orderBook);
-    }
-}
-
+//Add Order Benchmarks
 BENCHMARK(BM_AddOrder_same_price);
 BENCHMARK(BM_AddOrder_RandomPrice_Range_3);
 BENCHMARK(BM_AddOrder_RandomPrice_Range_10);
 BENCHMARK(BM_AddOrder_RandomPrice_Range_10_DB10k);
-BENCHMARK(BM_Cancel1_Random_Order_DB1k);
-BENCHMARK(BM_Cancel1_Random_Order_DB10k);
-BENCHMARK(BM_Cancel1_Random_Order_DB100k);
+
+//Cancel Order Benchmarks
+BENCHMARK(BM_Cancel1_Random_Order_DB_)->RangeMultiplier(10)->Range(100, 100000);
+
+//Get Best Bid Benchmarks
+BENCHMARK(BM_GetBestBid_bidDB_)->RangeMultiplier(10)->Range(100, 100000);
+
+//Get Ask Volume between Prices Benchmarks
+BENCHMARK(BM_GetAskVolume_askDB_)->RangeMultiplier(10)->Range(100, 100000);
 
 
 BENCHMARK_MAIN();
